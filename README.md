1784d7da3bd7faead8704c71d9042292099203c6
Konstantin Grigorchuk <kgrigorchuk@i-novus.ru>
2023-03-22 15:37:18 +0300

    Fixes geobde/useRerender#7 Component <name> rerendered" message logs on mount and before "Mounted" message

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
 # React hook to detect component mount, unmount & re-renders.
 
-## Installation
+See [Demo](https://codesandbox.io/s/gallant-hopper-jzjp2w?file=/src/Button.tsx)
 
-Using `npm`:
+## Install
 
 ```bash
 npm i use-is-rerender --save
 ```
 
+```bash
+yarn add use-is-rerender
+```
+
 ## Usage
 
 ```javascript
-import { useRerender } from "use-is-rerender";
+import { useRerender } from 'use-is-rerender'
 
 const Button = () => {
-  useRerender();
-  return <button>Click</button>;
-};
+  useRerender()
+  return <button>Click</button>
+}
 ```
 
-<img src="https://github.com/geobde/useRerender/blob/main/demo.png" width="900" height="500">
+<img src="https://github.com/geobde/useRerender/blob/main/demo.jpg" width="900" height="500">
+
+## Available hook options
+
+Option   | Acceptable&nbsp;Values                                                       | Details
+---------|------------------------------------------------------------------------------|---
+logProps | <ul><li><code>true</code>&nbsp;**(default)**</li><li><code>false</code></ul> | Can disable props logging    
+
+## Production mode
+Hook not working when `NODE_ENV==='production'`
+
 
 ## Read More
 [React Performance](https://medium.com/@geobde/react-performance-91d84678c613)
--- src/useRerender.ts
+++ src/useRerender.ts
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-export function useRerender() {
+interface IUseRerender {
+    logProps?: boolean
+}
+
+export function useRerender(config: IUseRerender = {}) {
     if (process.env.NODE_ENV === 'production') {
         return
     }
 
+    const logProps = config.logProps ?? true
+
     // @ts-ignore
     const owner = React?.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED?.ReactCurrentOwner?.current
     const name = owner?.type?.name ? `<${owner.type.name} />` : ''
-    const props = owner?.pendingProps
 
-    React.useEffect(() => {
-        console.log(`Component ${name} mounted`)
+    const props = owner?.pendingProps as object
+    const propsMessage = logProps ? ` with props: ${JSON.stringify(props)}` : ''
+    const prevPropsRef = useRef<null | object>(null)
+
+
+    useEffect(() => {
+        console.log(`Component ${name} mounted${propsMessage}`)
         return () => {
             console.log(`Component ${name} unmounted`)
         }
-    }, [name])
+    }, [])
 
-    console.log(`Component ${name} rerender with props: ${JSON.stringify(props)}`)
+    if (prevPropsRef.current) {
+        console.log(`Component ${name} rerender${propsMessage}`)
+    }
+
+    prevPropsRef.current = props
 }
